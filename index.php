4522ecf6a59d4f05eff8a1c09fb452fc175b0733
Zakihanafi <153246190+Zakihanafi@users.noreply.github.com>
2023-12-08 09:53:13 +0700

    commit1

---
+++ index.php
@@ -0,0 +1,67 @@
+
+      <?php include 'header.php';?>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img class="rounded-circle" width="550" height="300" src="https://logos-world.net/wp-content/uploads/2023/02/Premier-League-Symbol.png" alt="">
+          <h1 class="display-4">Premier league</h1>
+          <p class="lead">Liga Premier Inggris adalah liga tertinggi dalam sistem liga sepak bola di Inggris. Kompetisi ini diikuti oleh 20 klub, liga ini menerapkan sistem promosi dan degradasi dengan English Football League (EFL).</p>
+        </div>
+      </div>
+
+      <div class="container text-center" id="About"> 
+        <div class="row">
+            <div class="col">
+
+            </div>
+        </div>
+
+      <h1>About</h1>
+
+      <div class="row">
+        <div class="col text-justify">
+          Liga Utama Inggris atau Liga Premier Inggris adalah liga tertinggi dalam sistem liga sepak bola di Inggris. Kompetisi ini diikuti oleh 20 klub, liga ini menerapkan sistem promosi dan degradasi dengan English Football League (EFL).
+
+Premier League adalah sebuah perusahaan yang di dalamnya klub peserta liga bertindak sebagai pemegang saham. Musim kompetisi dimainkan dari bulan Agustus hingga Mei, di mana setiap tim bermain 38 pertandingan, dengan 19 pertandingan kandang dan 19 pertandingan tandang.[1]
+
+Kompetisi ini didirikan dengan nama awal FA Premier League (Liga Utama Inggris FA) pada 20 Februari 1992 setelah beberapa klub peserta Divisi Pertama Liga Inggris memutuskan untuk memisahkan diri dari liga tersebut yang telah didirikan pada tahun 1888, dan mengambil keuntungan dari kesepakatan hak siar televisi yang dinilai menguntungkan.[2] Kesepakatan tersebut bernilai 1 miliar poundsterling per musim 2013–2014, di mana Sky dan BT Group menjadi pemegang hak domestik untuk menyiarkan masing-masing 116 dan 38 pertandingan.[3] 22 klub bermain pada musim pertama liga.[4] Liga ini menghasilkan 2,2 miliar poundsterling per tahunnya sebagai hasil dari hak siar domestik dan internasional.[
+            
+            
+        </div>
+        <div class="col text-justify">
+          Liga ini menjadi liga olahraga dengan penonton terbanyak di dunia, dengan disiarkan di 212 wilayah ke 643 juta pemirsa di rumah[6] dan memiliki jumlah penonton potensial sebanyak 4,7 miliar.[7][8] Mayoritas stadion terisi penuh hampir mendekati kapasitasnya.[9] Liga Utama Inggris berada pada peringkat kedua dalam koefisien Liga UEFA berdasarkan pencapaian pada kompetisi Eropa dalam lima musim terakhir per 2019, dengan berada di belakang La Liga Spanyol.[10]
+
+49 tim telah bermain sepanjang penyelenggaraan, di mana 47 klub berbasis di Inggris dan 2 klub berasal dari Wales, menjadikan liga ini menjadi kompetisi lintas batas negara. Enam tim telah meraih gelar juara, yakni: Manchester United (13), Manchester City (7), Chelsea (5), Arsenal (3), Liverpool (1), Blackburn Rovers (1), dan Leicester City (1).
+        </div>
+     
+      </div>
+
+      <div class="card-deck mt-5 ml-5 mr-5">
+        <div class="card">
+          <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/800px-Chelsea_FC.svg.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Chelsea</h5>
+            <p class="card-text col text-justify">Chelsea Football Club /ˈtʃɛlsiː/ adalah sebuah klub sepak bola profesional yang bermarkas di Fulham, London. Didirikan pada 1905, klub ini kini berkompetisi di Liga Utama Inggris dan memainkan pertandingan kandang mereka di Stamford Bridge.[1][5] Chelsea merupakan salah satu klub tersukses di Inggris, dengan telah memenangkan lebih dari tiga puluh gelar juara kompetisi klub liga dan internasional.
+
+              Kesuksesan pertama Chelsea diraih saat meraih gelar juara Divisi 1 Liga Inggris pada 1955. Klub ini memenangkan gelar juara Piala Liga Inggris untuk pertama kali pada 1965, gelar juara Piala FA untuk pertama kali pada 1970, dan gelar juara kompetisi klub Eropa, Piala Winners Eropa untuk pertama kali pada 1971. </p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/id/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Manchester United</h5>
+            <p class="card-text col text-justify">Manchester United Football Club didirikan pada 1 Januari 1878 dengan nama Newton Heath LYR Football Club oleh pekerja perusahaan kereta api Lancashire and Yorkshire Railway di Newton Heath, Manchester.[1] Awalnya Newton Heath bermain dalam pertandingan persahabatan melawan tim dari departemen dan perusahaan perkeretaapian lain di lapangan mereka di North Road.[2] Pada tahun 1888, mereka dan beberapa klub di Inggris Utara dan Midlands lain mendirikan liga regional The Combination. Belum genap semusim berjalan, liga tersebut dibubarkan. Newton Heath kemudian bermain di Football Alliance, liga yang baru dibentuk pada waktu itu.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Newcastle_United_Logo.svg/1200px-Newcastle_United_Logo.svg.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Newcastle united</h5>
+            <p class="card-text col text-justify">Newcastle United Football Club adalah klub sepak bola profesional Inggris, yang berbasis di Newcastle upon Tyne, yang bermain di Liga Utama Inggris – divisi teratas sepak bola Inggris. Klub ini didirikan pada tahun 1892 melalui penggabungan Newcastle East End dan Newcastle West End. Mereka memainkan pertandingan kandang mereka di Stadion St James' Park di pusat kota Newcastle. Mengikuti persyaratan Taylor Report bahwa semua klub Liga Utama harus memiliki stadion all-seater, kemudian stadion tersebut dikembangkan pada pertengahan 1990-an dan saat ini memiliki kapasitas 52.305.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+      <?php include 'footerss.php';?>
+      <!-- Footer -->
