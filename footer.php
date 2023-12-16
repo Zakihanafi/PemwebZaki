df6a9ef35c2b4443992ff8071da5ba00d5fe8c91
Zakihanafi <153246190+Zakihanafi@users.noreply.github.com>
2023-12-16 21:38:54 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,129 +1,182 @@
-<footer class="text-center text-lg-start bg-white text-muted">
+<!-- Footer -->
+<footer class="bg-body-tertiary text-center">
+  <!-- Grid container -->
+  <div class="container p-4">
     <!-- Section: Social media -->
-    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-      <!-- Left -->
-      <div class="me-5 d-none d-lg-block">
-        <span>Get connected with us on social networks:</span>
-      </div>
-      <!-- Left -->
-  
-      <!-- Right -->
-      <div>
-        <a href="" class="me-4 link-secondary">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="" class="me-4 link-secondary">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="" class="me-4 link-secondary">
-          <i class="fab fa-google"></i>
-        </a>
-        <a href="" class="me-4 link-secondary">
-          <i class="fab fa-instagram"></i>
-        </a>
-        <a href="" class="me-4 link-secondary">
-          <i class="fab fa-linkedin"></i>
-        </a>
-        <a href="" class="me-4 link-secondary">
-          <i class="fab fa-github"></i>
-        </a>
-      </div>
-      <!-- Right -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"
+        ><i class="https://www.facebook.com/premierleagueIndonesia"></i
+      ></a>
+
+      <!-- Twitter -->
+      <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+      <!-- Google -->
+      <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-google"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-instagram"></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-linkedin-in"></i
+      ></a>
+
+      ></a>
     </section>
     <!-- Section: Social media -->
-  
-    <!-- Section: Links  -->
+
+    <!-- Section: Form -->
     <section class="">
-      <div class="container text-center text-md-start mt-5">
-        <!-- Grid row -->
-        <div class="row mt-3">
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              <i class="fas fa-gem me-3 text-secondary"></i>Company name
-            </h6>
-            <p>
-              Pendeta Afin
+      <form action="">
+        <!--Grid row-->
+        <div class="row d-flex justify-content-center">
+          <!--Grid column-->
+          <div class="col-auto">
+            <p class="pt-2">
+              <strong>Sign up for our newsletter</strong>
             </p>
           </div>
-          <!-- Grid column -->
-  
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              Products
-            </h6>
-            <p>
-              <a href="#!" class="text-reset">Angular</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">React</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Vue</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Laravel</a>
-            </p>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-md-5 col-12">
+            <!-- Email input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+              <input type="email" id="form5Example24" class="form-control" />
+              <label class="form-label" for="form5Example24">Email address</label>
+            </div>
           </div>
-          <!-- Grid column -->
-  
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              Useful links
-            </h6>
-            <p>
-              <a href="#!" class="text-reset">Pricing</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Settings</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Orders</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Help</a>
-            </p>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-auto">
+            <!-- Submit button -->
+            <button data-mdb-ripple-init type="submit" class="btn btn-outline mb-4">
+              Subscribe
+            </button>
           </div>
-          <!-- Grid column -->
-  
-          <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-            <p><i class="fas fa-home me-3 text-secondary"></i> Ukraine,  10012, </p>
-            <p>
-              <i class="fas fa-envelope me-3 text-secondary"></i>
-              Yadikuda@gmail.com
-            </p>
-            <p><i class="fas fa-phone me-3 text-secondary"></i> + 045454545454</p>
-            <p><i class="fas fa-print me-3 text-secondary"></i> + 0786666666</p>
-          </div>
-          <!-- Grid column -->
+          <!--Grid column-->
         </div>
-        <!-- Grid row -->
-      </div>
+        <!--Grid row-->
+      </form>
     </section>
-    <!-- Section: Links  -->
-  
-    <!-- Copyright -->
-    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
-      © 2023 Copyright:.
-      <a class="text-reset fw-bold" href="https://mdbootstrap.com/">PendetaAfin.com</a>
-    </div>
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
+    <!-- Section: Form -->
 
+    <!-- Section: Text -->
+    <section class="mb-4">
+      <p>
+       Premier League
+      </p>
+    </section>
+    <!-- Section: Text -->
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
-</html>
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Premier league</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a class="Home" href="#!">https://www.premierleague.com/home</a>
+            </li>
+            <li>
+              <a class="Fixtures" href="#!">https://www.premierleague.com/fixtures</a>
+            </li>
+            <li>
+              <a class="Result" href="#!">https://www.premierleague.com/results?team=FIRST</a>
+            </li>
+            <li>
+              <a class="Tables" href="#!">https://www.premierleague.com/tables</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Community</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a class="Wider Football" href="#!">https://www.premierleague.com/footballandcommunity/wider-football</a>
+            </li>
+            <li>
+              <a class="charitable fund" href="#!">https://www.premierleague.com/footballandcommunity/premier-league-charitable-fund</a>
+            </li>
+            <li>
+              <a class="No Room for Racism" href="#!">https://premierleague.com/footballandcommunity/NoRoomForRacism</a>
+            </li>
+            <li>
+              <a class="Poppy" href="#!">https://www.premierleague.com/footballandcommunity/poppy</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">About</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a class="Overview" href="#!">https://www.premierleague.com/about</a>
+            </li>
+            <li>
+              <a class="Partners" href="#!">https://www.premierleague.com/about/partners</a>
+            </li>
+            <li>
+              <a class="Governance" href="#!">https://www.premierleague.com/about/governance</a>
+            </li>
+            <li>
+              <a class="Safeguarding" href="#!">https://www.premierleague.com/about/safeguarding</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Social</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a class="PL on YouTube" href="#!">https://www.youtube.com/premierleague</a>
+            </li>
+            <li>
+              <a class="PL on Tik Tok" href="#!">https://www.tiktok.com/@premierleague</a>
+            </li>
+            <li>
+              <a class="PL on X" href="#!">https://twitter.com/premierleague</a>
+            </li>
+            <li>
+              <a class="PL on Instagram" href="#!">https://www.instagram.com/premierleague</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2024 Copyright:
+    <a class="text-reset fw-bold" href="https://www.premierleague.com/matchweek/12285/blog">Premierleague.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
